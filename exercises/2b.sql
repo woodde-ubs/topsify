8f5f44614316d7edc1a6a54e3434920b85480889
Simon Haines <nimon@hotmail.co.uk>
2024-05-29 18:20:28 +0100

    Update 2b.sql

--- exercises/2b.sql
+++ exercises/2b.sql
@@ -1,3 +1,3 @@
--- Find the track_id of all tracks whose "speechiness" is greater than 0.5
+-- Find the id of all tracks whose "speechiness" is greater than 0.5
 -- but sort them by their danceability, with least danceable tracks first
 -- (Hint: use the features table)
