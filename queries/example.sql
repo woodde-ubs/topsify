7788b722316aff7b4054732345bce8e5b7c86a40
Simon Haines <nimon@hotmail.co.uk>
2024-05-29 18:02:52 +0100

    Update example.sql

--- queries/example.sql
+++ queries/example.sql
@@ -1,12 +1,12 @@
 -- To execute the query, open a terminal and run:
 -- sqlite3 db/db.sqlite < queries/example.sql
 
--- To make a report, add an output file:
--- sqlite3 db/db.sqlite < queries/example.sql > reports/example.txt
-
 SELECT name, duration_ms
 FROM tracks
 LIMIT 5;
+
+-- To make a report, add an output file:
+-- sqlite3 db/db.sqlite < queries/example.sql > reports/example.txt
 
 -- Hint: to make a JSON report, add ".mode json" to the top of the .sql file
 -- then output to > reports.json
